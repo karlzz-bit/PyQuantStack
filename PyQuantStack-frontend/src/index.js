75a0a6a5e1ec9316302f01a18f42c85493568a29
karlzz-bit <194922620+karlzz-bit@users.noreply.github.com>
2025-03-04 10:24:30 +0800

    Create index.js

---
+++ PyQuantStack-frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// 渲染主 App 组件到根节点
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
